522a916f3e814334f74576e7756ce4b944dd5647
Kevin <kevin@bear.com>
2023-05-09 07:17:23 +0800

    早起的🦟被拍死

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,17 @@ fn main() {
     let num:u16 = 255;
     println!("num={}",num);
     println!("Hello, world!");
+    let s1 = String::from("hello");
+    let s3 = takes_ownership(s1);
+    println!("s3 {:?}", s3); // error: value borrowed here after move
+
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
+fn takes_ownership(some_string: String)->String { // some_string 进入作用域
+    println!("takes_ownership {}", some_string);
+    some_string
+} //
